8f1277bf5e56e65d500a4f155355bb6f7d203880
MSS1410 <151957312+MSS1410@users.noreply.github.com>
2025-04-14 20:04:06 +0200

    Update NotFound.jsx

---
+++ src/pages/404/NotFound.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './NotFound.css'
+
+const notFound = () => {
+  return (
+    <div className='NotFound'>
+      <h1>Sorry, Not Found - 404 - </h1>
+    </div>
+  )
+}
+
+export default notFound
